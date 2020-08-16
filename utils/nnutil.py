cf182513184ca2f33da83215783bbe53d70b9218
xufan.zhang <sunshinezxf@hotmail.com>
2020-08-16 14:44:37 +0800

    add implementation for detect implementation

--- utils/nnutil.py
+++ utils/nnutil.py
@@ -12,3 +12,9 @@ def train_lenet():
     print(x_train[0] / 255)
     lenet = LeNet(model_filename='lenet_mnist_1.h5', epochs=10, input_shape=(28, 28, 1), weight_decay=1e-3)
     lenet.train(x_train / 255, y_train)
+
+
+def top_equal(k, o_1, o_2):
+    print("top %s", k)
+    print(o_1 == o_2)
+    return
