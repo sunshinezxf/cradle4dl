f4904684d5bb50ed3caef67b7b7e7e8ccd05b525
wen <1280252178@qq.com>
2021-01-24 13:15:12 +0800

    预下载

--- utils/download/cifar10_download.py
+++ utils/download/cifar10_download.py
@@ -4,7 +4,7 @@ from keras.datasets import cifar10
 
 def download_cifar10():
     print("开始下载数据集cifar10")
-    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    cifar10.load_data()
     print("完成下载数据集cifar10")
     print(x_train.shape)
 
