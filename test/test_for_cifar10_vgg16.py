5c4e2e5866bede01c2bb0df71370acdf942e9f69
wen <1280252178@qq.com>
2021-01-29 15:51:36 +0800

    cradle for vgg16

--- test/test_for_cifar10_vgg16.py
+++ test/test_for_cifar10_vgg16.py
@@ -1,7 +1,7 @@
 
 import os
 from keras.models import load_model
-from keras.datasets import mnist
+from keras.datasets import cifar10
 import cradle
 
 
@@ -19,14 +19,14 @@ def test4():
     # nnutil.train_lenet()
 
     # 加载模型
-    model_dir = basedir + "/network/models/lenet/"
-    model_path = [model_dir + "lenet_mnist_1.h5", model_dir + "lenet_mnist_2.h5"]
+    model_dir = basedir + "/network/models/vgg16/"
+    model_path = [model_dir + "vgg16_cifar10_tensorflow.h5", model_dir + "vgg16_cifar10_theano.h5"]
     print("load model")
     mo = load_model(model_path[0])
     mc = load_model(model_path[1])
 
     # 数据获取
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
     x_test = x_test.reshape(-1, 28, 28, 1)
 
     # detect
@@ -41,3 +41,7 @@ def test4():
     else:
         print("认为结果符合预期")
         print(inconsistency_count, "/", len(x_test))
+
+
+if __name__ == '__main__':
+    test4()
